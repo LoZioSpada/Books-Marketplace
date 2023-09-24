0667ca3436cd9ed9a10048f856616f45baa72d8d
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-09-24 14:15:24 +0200

    Books-Marketplace
    - Implementato la funzione che rimuove un libro;

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const getBooks = () => {
                             <p class="card-text">Category: ${book.category}</p>
                             <p class="card-text"> Price: ${book.price}</p>
                             <div class="d-flex justify-content-around align-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" onclick="rimuoviLibro('${book.asin}')">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                 </svg>
 
@@ -90,4 +90,11 @@ const svuotaCarrello = () => {
     document.querySelectorAll('.card').forEach(card => card.style.border = 'none')
     const totale = document.querySelector('.totalCart')
     totale.innerText = '0'
+}
+
+
+// FUNZIONE PER RIMUOVERE ("SALTARE(?)") UN LIBRO
+const rimuoviLibro = (asin) => {
+    const book = document.querySelector('#book_' + asin)
+    book.style.display = 'none'
 }

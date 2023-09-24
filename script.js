8135d71b8dd901513d5e36f8dfe8c820fba95452
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-09-24 12:16:49 +0200

    Books-Marketplace
    - Cambiato le info da inglese a italiano;
    - Implementato la funzione per effettuare la ricerca del libro tramite la barra di ricerca (input);

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ const getBooks = () => {
                     <div class="card mb-5">
                         <img src='${book.img}' class="card-img-top">
                         <div class="card-body p-1">
-                            <h5 class="card-title">'${book.title}'</h5>
-                            <p class="card-text">Category: '${book.category}'</p>
-                            <p class="card-text"> Price: € '${book.price}'</p>
+                            <h5 class="card-title">${book.title}</h5>
+                            <p class="card-text">Category: ${book.category}</p>
+                            <p class="card-text"> Price: € ${book.price}</p>
                             <div class="d-flex justify-content-around align-items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -38,4 +38,21 @@ const getBooks = () => {
 
 window.onload = () => {
     getBooks()
+}
+
+
+//FUNZIONE PER FAR UTILIZZARE LA RICERCA E DEVE FILTRARE IN BASE ALLE PRIME 3 LETTERE SCRITTE
+const searchBook = (event) => {
+    let query = event.target.value
+    let tuttiTitoli = document.querySelectorAll('.card-title')
+    console.log(query, tuttiTitoli[0].innerText.toLowerCase().includes(query.toLowerCase()))
+
+    tuttiTitoli.forEach((title) => {
+        const cardCorrente = title.parentElement.parentElement.parentElement
+        if (!title.innerText.toLowerCase().includes(query.toLowerCase())) {
+            cardCorrente.style.display = 'none'
+        } else {
+            cardCorrente.style.display = 'block'
+        }
+    })
 }

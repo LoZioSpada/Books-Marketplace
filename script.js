4f6bbd3a52b3eb091df5810645b2f5e7ea7541df
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-09-28 18:48:59 +0200

    Books-Marketplace
    - Collegato la pagina index.html a dettagli.html utlizzando gli URLSearchParams;
    - Stilato la pagina dettagli.html;

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ const url = "https://striveschool-api.herokuapp.com/books"
 
 const ricerca = document.querySelector('#ricerca')
 
+let contenuto = document.querySelector('.book .row')
+
+contenuto.innerHTML =   // CARICAMENTO DELLA PAGINA
+`
+<div class="libro">
+	<div class="libro__pg-shadow"></div>
+	<div class="libro__pg"></div>
+	<div class="libro__pg libro__pg--2"></div>
+	<div class="libro__pg libro__pg--3"></div>
+	<div class="libro__pg libro__pg--4"></div>
+	<div class="libro__pg libro__pg--5"></div>
+</div>
+`
+
 // FUNZIONE PER FAR APPARIRE TUTTI I LIBRI COME CARD NEL BODY
 const getBooks = () => {
     fetch(url)
         .then(response => response.json())
         .then(result => {
-            let contenuto = document.querySelector('.book .row')
             contenuto.innerHTML = result.map((book) => {
                 // CREAZIONE CARD
                 return `
@@ -16,12 +29,19 @@ const getBooks = () => {
                         <img src='${book.img}' class="card-img-top">
                         <div class="card-body p-1">
                             <h5 class="card-title">${book.title}</h5>
-                            <p class="card-text">Category: ${book.category}</p>
-                            <p class="card-text"> Price: ${book.price}</p>
-                            <div class="d-flex justify-content-around align-items-center">
+                            <p class="card-text">Categoria: ${book.category}</p>
+                            <p class="card-text"> Prezzo: ${book.price}</p>
+                            <div class="d-flex justify-content-around align-items-center pb-1 px-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" onclick="rimuoviLibro('${book.asin}')">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                 </svg>
+
+                                <a href="dettagli/dettagli.html?id=${book.asin}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width=30" height="30" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                </svg>
+                                </a>
 
                                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16" onclick="aggiungiAlCarrello('${book.title}', '${book.price}', '${book.asin}' )">
                                 <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
@@ -34,6 +54,7 @@ const getBooks = () => {
             }).join('')
         })
         .catch(error => console.log(error))
+        .finally(() => {contenuto.querySelector('.libro').remove()})
 }
 
 window.onload = () => {
